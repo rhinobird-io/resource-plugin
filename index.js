1f8e74fbcb943569505784df8d7645acb23028cd
RonaldoLee <ljxnegod@hotmail.com>
2015-06-18 18:50:39 +0800

    Get resource by id.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var restify = require('restify');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
+var ObjectId = Schema.Types.ObjectId;
+
+var ResourceBookingSchema = new Schema({
+    fromTime: Schema.Types.Date,
+    toTime: Schema.Types.Date
+});
+
+var ResourceBooking = mongoose.model('ResourceBooking', ResourceBookingSchema);
 
 var ResourceSchema = new Schema({
-    id: ObjectId,
-    title: String
+    name: String,
+    category: String,
+    description: String
 });
+
+var Resource = mongoose.model('Resource', ResourceSchema);
 
 mongoose.connect('mongodb://localhost/resource');
 
@@ -19,8 +29,13 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-mongoose.model('Resource', ResourceSchema);
-var Resource = mongoose.model('Resource');
+function getResourceById(req, res, next) {
+    var id = req.params.id;
+    console.log(id);
+    Resource.findById(id, function(err, doc) {
+        return res.send(JSON.stringify(doc));
+    });
+}
 
 function getResources(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -28,7 +43,7 @@ function getResources(req, res, next) {
     // .find() without any arguments, will return all results
     // the `-1` in .sort() means descending order
     Resource.find().lean().exec(function (err, data) {
-        return res.end(JSON.stringify(data));
+        return res.send(JSON.stringify(data));
     });
 }
 
@@ -42,7 +57,7 @@ function postResources(req, res, next) {
 }
 
 server.get('/resources', getResources);
-
+server.get('/resources/:id', getResourceById);
 server.post('/resources', postResources);
 
 var port = process.env.PORT || 6100;
